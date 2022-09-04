fcde9422e3e64ee68ef45ac07c4e69b2c1043dbc
Aditya Vyas <vyasaditya41@yahoo.in>
2022-09-04 14:34:31 +0530

    add: typeorm and intialize the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: 'secret',
+    database: 'task-management',
+    autoLoadEntities: true,
+    synchronize: true
+  })],
 })
 export class AppModule {}
