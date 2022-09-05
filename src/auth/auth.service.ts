9db35e84a5c245bbf4f01866c6f3a1b0cd6e7abc
Aditya Vyas <vyasaditya41@yahoo.in>
2022-09-06 00:43:02 +0530

    add: new setup to onboard user for the app

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>    
+    ){}
+    
+        
+}
