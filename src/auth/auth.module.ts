a0239cbd238bb5fcc5dbe013c821c6c62e1e34e8
Aditya Vyas <vyasaditya41@yahoo.in>
2022-09-10 19:29:39 +0530

    chore: setting up passport and jwt module for auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,15 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    JwtModule.register({ secret: 'topsecret51', signOptions: {
+      expiresIn: 3600,
+    }}),
     TypeOrmModule.forFeature([User])
   ],
   providers: [AuthService],
